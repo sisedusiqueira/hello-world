e6b74aef06d1413dac4cfc9c0cfab3e8376406e8
Lizandra Anjos <138258723+sisedusiqueira@users.noreply.github.com>
2023-07-12 08:04:36 -0300

    Create algoritmos.py
    Adicionando o arquivo algoritmos ao diretório hello-world.

---
+++ algoritmos.py
@@ -0,0 +1,6 @@
+# Primeiros passos para iniciar na programação de computadores. 
+
+# Conceito de Algoritmo
+
+<h1>Definição de algoritmos<h1><br>
+<p>Algoritmos são conjutos de passos finitos e organizados que, quando executados, resolvem um determinado problema.
